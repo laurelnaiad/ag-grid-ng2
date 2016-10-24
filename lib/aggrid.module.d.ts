67a6b2cbafaf85869412eb0c3ae69dd9670c4ff3
Daphne Maddox <laurelnaiad@gmail.com>
2016-10-24 10:50:28 -0700

    built2

--- lib/aggrid.module.d.ts
+++ lib/aggrid.module.d.ts
@@ -1,7 +1,9 @@
 // ag-grid-ng2 v6.0.3
+import { AgComponentFactory } from './agComponentFactory';
+import { Ng2FrameworkFactory } from './ng2FrameworkFactory';
 export declare class AgGridModule {
     static forRoot(): {
         ngModule: typeof AgGridModule;
-        providers: any[];
+        providers: (typeof AgComponentFactory | typeof Ng2FrameworkFactory | any[])[];
     };
 }
