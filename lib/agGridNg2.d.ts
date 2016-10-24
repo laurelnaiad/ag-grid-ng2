3339fda7df1caab19f1ef1838db7d57796014789
Daphne Maddox <laurelnaiad@gmail.com>
2016-10-24 10:40:24 -0700

    built

--- lib/agGridNg2.d.ts
+++ lib/agGridNg2.d.ts
@@ -1,10 +1,11 @@
 // ag-grid-ng2 v6.0.3
-import { ViewContainerRef, ElementRef } from '@angular/core';
+import { ViewContainerRef, ElementRef, NgZone } from '@angular/core';
 import { GridApi, ColumnApi } from 'ag-grid/main';
 import { Ng2FrameworkFactory } from "./ng2FrameworkFactory";
 export declare class AgGridNg2 {
     private viewContainerRef;
     private ng2FrameworkFactory;
+    private zone;
     private _nativeElement;
     private _initialised;
     private _destroyed;
@@ -12,7 +13,8 @@ export declare class AgGridNg2 {
     private gridParams;
     api: GridApi;
     columnApi: ColumnApi;
-    constructor(elementDef: ElementRef, viewContainerRef: ViewContainerRef, ng2FrameworkFactory: Ng2FrameworkFactory);
+    constructor(elementDef: ElementRef, viewContainerRef: ViewContainerRef, ng2FrameworkFactory: Ng2FrameworkFactory, zone: NgZone);
+    private escapeZone(timeoutOrInterval, period, action);
     ngOnInit(): void;
     ngOnChanges(changes: any): void;
     ngOnDestroy(): void;
--- lib/aggrid.module.d.ts
+++ lib/aggrid.module.d.ts
@@ -1,9 +1,7 @@
 // ag-grid-ng2 v6.0.3
-import { AgComponentFactory } from './agComponentFactory';
-import { Ng2FrameworkFactory } from './ng2FrameworkFactory';
 export declare class AgGridModule {
     static forRoot(): {
         ngModule: typeof AgGridModule;
-        providers: (typeof AgComponentFactory | typeof Ng2FrameworkFactory | any[])[];
+        providers: any[];
     };
 }
